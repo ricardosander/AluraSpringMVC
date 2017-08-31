0fccf5ca90b9e57be67ba7d016241822020c2a04
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-31 00:31:29 -0300

    Tornando DAO transacional.

--- src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -2,11 +2,13 @@ package br.com.casadocodigo.loja.dao;
 
 import br.com.casadocodigo.loja.model.Produto;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
+@Transactional
 public class ProdutoDAO {
 
     @PersistenceContext
