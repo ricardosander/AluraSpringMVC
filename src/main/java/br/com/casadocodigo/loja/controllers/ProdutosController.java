14fa7a8ff28a233a79cc91c9aa3b6c00bc622e85
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-31 00:01:21 -0300

    Criando método para atender a requisição de post para cadastrar um novo produto.

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.controllers;
 
+import br.com.casadocodigo.loja.model.Produto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,12 @@ public class ProdutosController {
     @RequestMapping("/produtos/form")
     public String form() {
         return "produtos/form";
+    }
+
+    @RequestMapping("/produtos")
+    public String gravar(Produto produto) {
+        System.out.println(produto);
+        return "produtos/ok";
     }
 
 }
