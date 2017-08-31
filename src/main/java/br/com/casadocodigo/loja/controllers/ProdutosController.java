d8503f5f1a186b3c60ea46023fbb032d963749b8
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-31 00:22:46 -0300

    Adicionando DAO como injeção de dependência e utilizando-a.

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,11 +1,16 @@
 package br.com.casadocodigo.loja.controllers;
 
+import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.model.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProdutosController {
+
+    @Autowired
+    private ProdutoDAO produtoDAO;
 
     @RequestMapping("/produtos/form")
     public String form() {
@@ -14,7 +19,9 @@ public class ProdutosController {
 
     @RequestMapping("/produtos")
     public String gravar(Produto produto) {
-        System.out.println(produto);
+
+        this.produtoDAO.gravar(produto);
+
         return "produtos/ok";
     }
 
