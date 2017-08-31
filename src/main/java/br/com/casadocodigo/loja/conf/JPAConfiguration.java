9eb5d01d2becd8170bc00ada529ce2d685a32f16
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-31 00:13:49 -0300

    Criando arquivo de configuração do JPA.

---
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -0,0 +1,42 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import java.util.Properties;
+
+public class JPAConfiguration {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+
+        LocalContainerEntityManagerFactoryBean factoryBean;
+        factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("root");
+        dataSource.setPassword("123");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+        factoryBean.setDataSource(dataSource);
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        factoryBean.setJpaProperties(properties);
+
+        factoryBean.setPackagesToScan("br.com.casadocodigo.loja.model");
+
+        return factoryBean;
+    }
+
+}
