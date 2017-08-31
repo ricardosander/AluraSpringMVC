37798fb4ed6ad0c8828954cb99d9ff15e346e700
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-30 22:07:46 -0300

    Alterando método do controller para retornar view.

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public void index() {
-        System.out.println("Entrando na home do CDC.");
+    public String index() {
+        return "home";
     }
 
 }
