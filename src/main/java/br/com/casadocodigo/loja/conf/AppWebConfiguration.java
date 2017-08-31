33f5e6773fbd0585b9fddc732815456f7020c1d4
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-30 22:05:57 -0300

    Criando método que retorna o resolvedor de views.

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,11 +1,24 @@
 package br.com.casadocodigo.loja.conf;
 
 import br.com.casadocodigo.loja.controllers.HomeController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses = HomeController.class)
 public class AppWebConfiguration {
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+
+        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
 
 }
