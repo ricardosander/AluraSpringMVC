f3edbe1bc6c5f2fbc97c04124ed96c6670dfd007
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-09-02 15:04:32 -0300

    Alterando comportamento e retorno do  método form.

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,9 +2,11 @@ package br.com.casadocodigo.loja.controllers;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.model.Produto;
+import br.com.casadocodigo.loja.model.TipoPreco;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProdutosController {
@@ -13,8 +15,13 @@ public class ProdutosController {
     private ProdutoDAO produtoDAO;
 
     @RequestMapping("/produtos/form")
-    public String form() {
-        return "produtos/form";
+    public ModelAndView form() {
+
+        ModelAndView modelAndView = new ModelAndView("produtos/form");
+
+        modelAndView.addObject("tipos", TipoPreco.values());
+
+        return modelAndView;
     }
 
     @RequestMapping("/produtos")
