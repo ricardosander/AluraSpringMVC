daa3fad5690b98170e6c85be46fa6b8d7e1673d5
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-09-03 21:16:06 -0300

    Aplicando validações aos cadastro de produto.

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -3,13 +3,18 @@ package br.com.casadocodigo.loja.controllers;
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.model.Produto;
 import br.com.casadocodigo.loja.model.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -18,6 +23,11 @@ public class ProdutosController {
 
     @Autowired
     private ProdutoDAO produtoDAO;
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(new ProdutoValidation());
+    }
 
     @RequestMapping(method = RequestMethod.GET, value = "/form")
     public ModelAndView form() {
@@ -30,7 +40,11 @@ public class ProdutosController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
+    public ModelAndView gravar(@Valid Produto produto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return this.form();
+        }
 
         this.produtoDAO.gravar(produto);
 
